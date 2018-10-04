cd4d5a3b2a7a162261484aba6fa3e683f556dcbf
Benjamin Joyce <benjaminandrewjoyce@gmail.com>
2018-10-04 15:15:52 +1000

    make better

--- client/src/App.js
+++ client/src/App.js
@@ -71,32 +71,48 @@ class App extends Component {
         <div className="M-floating-div container">
           <div>
             <h1>Fly without Limits</h1>
-            <span>
-              Search and book a private aircraft instantly...
-              <h5 />
-              Select from a wide range of private aircraft operators around the
-              world
-            </span>
+            <h3>Coming Soon</h3>
+            <div className="slgn2">
+              <h5> Search and book a private aircraft instantly...</h5>
+              <span>
+                Select from a wide range of private aircraft operators around
+                the world
+              </span>
+            </div>
           </div>
         </div>
 
         <div className="footer">
           <div className="row M-footer">
             <div className="container">
-              <div className="col m4 s6">
+              <div className="col m4 s12 container">
                 <a href="/">
                   <img id="footer-logo" src={logoB} alt="" srcset="" />
                 </a>
-                <span className="hide-on-med-and-up">
-                  Level 3, 315 Brunswick Street Fortitude Valley, Qld. 4006
-                </span>
+                <div>
+                  <span className="hide-on-med-and-up center-align address">
+                    Level 3, 315 Brunswick Street Fortitude Valley, Qld. 4006
+                  </span>
+                </div>
+                <div className="hide-on-med-and-up" id="M-social-sm">
+                  <a href="https://www.facebook.com/monarcglobal/">
+                    <FontAwesomeIcon icon={faFacebookSquare} size="3x" />
+                  </a>
+
+                  <a href="https://www.instagram.com/monarcglobal/">
+                    <FontAwesomeIcon icon={faInstagram} size="3x" />
+                  </a>
+                  <a href="https://twitter.com/Monarcglobal?lang=en">
+                    <FontAwesomeIcon icon={faTwitterSquare} size="3x" />
+                  </a>
+                </div>
               </div>
               <div className="col m4 hide-on-small-only">
-                <span>
+                <span className="address">
                   Level 3, 315 Brunswick Street Fortitude Valley, Qld. 4006
                 </span>
               </div>
-              <div className="col m4 s6" id="M-social">
+              <div className="col m4 s6 hide-on-small-only" id="M-social">
                 <a href="https://www.facebook.com/monarcglobal/">
                   <FontAwesomeIcon icon={faFacebookSquare} size="4x" />
                 </a>
@@ -104,13 +120,14 @@ class App extends Component {
                 <a href="https://www.instagram.com/monarcglobal/">
                   <FontAwesomeIcon icon={faInstagram} size="4x" />
                 </a>
-                <a href="/">
+                <a href="https://twitter.com/Monarcglobal?lang=en">
                   <FontAwesomeIcon icon={faTwitterSquare} size="4x" />
                 </a>
               </div>
             </div>
-            <hr />
-            <div className="tm">Trademark Monarc 2017</div>
+            <div className="m-flex-center">
+              <span className="tm">Trademark Monarc 2017</span>
+            </div>
           </div>
         </div>
       </div>
